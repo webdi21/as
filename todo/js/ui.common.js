5a873d8deb3e5acd8fdbc0f8064a6083ecb90e79
ONESTORECORP\1000139 <webdi21@onestorecorp.com>
2017-03-23 18:19:20 +0900

    $http 활용

--- todo/js/ui.common.js
+++ todo/js/ui.common.js
@@ -1,36 +1,28 @@
 var model = {
 	user:'Adam'
-	/* items:[
-		{action:'Buy Flowers',done:false},
-		{action:'Get Shoes',done:false},
-		{action:'Collect Tickets',done:true},
-		{action:'Call Joe',done:false}
-	] */
-};
+}
 
-var todo = angular.module('todoApp', []);
+var todo = angular.module('todoApp',[]);
 
-todo.filter("checkedItems", function() {
+todo.filter('checkedItems', function() {
 	return function(items, showComplete) {
 		var resultArr = [];
 		angular.forEach(items, function(item) {
-			if ( item.done == false || showComplete == true )  {
+			if (item.done == false || showComplete == true) {
 				resultArr.push(item);
 			}
-		});
+		})
 		return resultArr;
 	}
-});
+})
 
-todo.controller('todoCtrl',function($scope, $http, $location) {
-	$scope.todo = {
-		user:model.user
-	}
+todo.controller('todoCtrl', function($scope, $http) {
+	$scope.todo = model;
 
 	$scope.incompleteCount = function() {
 		var count = 0;
 		angular.forEach($scope.todo.items, function(item) {
-			if ( !item.done ) { count++ };
+			if (item.done == false) { count++ }
 		})
 		return count;
 	}
@@ -41,42 +33,52 @@ todo.controller('todoCtrl',function($scope, $http, $location) {
 
 	$scope.addNewItem = function(actionText) {
 		if ( actionText ) {
-			//$scope.todo.items.push({action:actionText,done:false});
-			$http.post("http://10.202.66.33:5500/todo/", {
-				action: actionText,
-				done: false
+			// $scope.todo.items.push({action:actionText, done:false})
+			
+			$http.post('http://10.202.66.33:5500/todo/',{
+				action:actionText, done:false
 			})
 			.then(function onSuccess(response) {
-				// 데이터 추가가 성공한 경우
 				$scope.todo.items.push(response.data);
 			},function onError(err) {
-				// 데이터 추가가 실패한 경우
-				return alert(err.message || (err.errors && err.errors.completed) || "an error occurred");
+				console.log(err.message);
 			})
+
 			$scope.actionText = '';
 		} else {
-			alert('제목을 입력해 주세요');
+			alert('제목을 입력하세요');
 		}
 	}
 
 	$scope.changeCompleted = function(todo) {
-		$http.put('http://10.202.66.33:5500/todo/' + todo.id, {
-			done: todo.done
+		$http.post('http://10.202.66.33:5500/todo/'+todo.id,{
+			done:todo.done
 		})
 		.then(function onSuccess(response) {
-			// 업데이트가 성공한 경우
 		},function onError(err) {
-			// 업데이트가 실패한 경우
-			return alert(err.message || (err.errors && err.errors.completed) || "an error occurred");
-		});
-	};
+			console.log(err.message);
+		})
+	}
 
-	$http.get("http://10.202.66.33:5500/todo/")
+	$scope.deleteCompleted = function(todo) {
+		$http.delete('http://10.202.66.33:5500/todo/'+todo.id)
+		.then(function onSuccess(response) {
+			var count ;
+			angular.forEach($scope.todo.items, function(item,i) {
+				if (item.id == todo.id) {
+					count = i;
+				}
+			});
+			$scope.todo.items.splice(count,1);
+		},function onError(err) {
+			console.log(err.message);
+		})
+	}
+
+	$http.get('http://10.202.66.33:5500/todo/')
 	.then(function onSuccess(response) {
-		// 읽어오기가 성공한 경우
 		$scope.todo.items = response.data;
-	},function onError(err){
-		// 읽어오기가 실패한 경우
-		return alert(err.message || (err.errors && err.errors.completed) || "an error occurred");
-	});
-});
+	},function onError(err) {
+		console.log(err.message);
+	})
+})
